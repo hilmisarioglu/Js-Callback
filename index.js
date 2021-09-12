83ee37fe2c83a501c4694b6ad23b7c0de990aa4d
hilmisarioglu <77903324+hilmisarioglu@users.noreply.github.com>
2021-09-12 21:59:46 +0200

    2

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const movement = [100, -200, 50, -120];
-
+arrDeposits=[];
+arrWithdraws=[];
 // You deposited $..
 // You withdrew $..
 
 movement.forEach(element => {
     if (element > 0) {
         console.log(`You deposited $ ${element}`)
+        arrDeposits.push(element)
     } else {
         console.log(`You withdrew $ ${element}`)
+        arrWithdraws.push(element)
     }
 });
 
-
+console.log('Deposits are :',arrDeposits)
+console.log('Withdraws are :',arrWithdraws)
 // -------------------------------------------------------
 
